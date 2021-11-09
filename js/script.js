dd4e010a80bcf6d8e3ad243d9b124c63b1f74f1a
GinoGreen <luigiverde97@gmail.com>
2021-11-09 15:51:01 +0100

    commit routine

--- js/script.js
+++ js/script.js
@@ -3,6 +3,34 @@
  * dopo 30 secondi chiedere all'utente 5 numeri
  * se i numeri corrispodono ai numeri random, output "hai indovinato 'TOT' numeri"
  */
+ const NUMBER_TO_PUSH = 5;
+ const randomNumbers = [];
 
-const randomNumbers = [];
 
+pushRandomNumbers();
+
+
+/**********
+ * FUNZIONI
+********* */
+
+function pushRandomNumbers() {
+      
+   while (randomNumbers.length < NUMBER_TO_PUSH) {
+      
+      const number = getRandomNumber(1, 100);
+
+      //controllo doppioni
+      if (!randomNumbers.includes(number)) {
+         randomNumbers.push(number);
+      }
+   }
+   // console.log(randomNumbers);
+      
+}
+
+
+function getRandomNumber(min, max) {
+   
+   return Math.floor(Math.random() * (max - min + 1) + min);
+}
