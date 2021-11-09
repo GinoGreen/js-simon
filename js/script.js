a3ddb5aebc7b15ee163c44c02dad3239cda4d75d
GinoGreen <luigiverde97@gmail.com>
2021-11-09 16:59:29 +0100

    controllo numeri ricordati

--- js/script.js
+++ js/script.js
@@ -5,17 +5,40 @@
  */
 const NUMBER_TO_PUSH = 5;
 const randomNumbers = [];
-
+let timeSeconds = 1;
+// numeri ricordati
+let strikes = 0;
 //Riempio l'array con numeri random
-pushRandomNumbers();
+pushNumbers();
 
+//stampo i numeri i html
 printNumbers();
+
+//faccio scorrere 30 sec
+console.log(timeSeconds);
+
+const countDown = setInterval(() => {
+   timeSeconds--;
+   console.log(timeSeconds);
+   if (timeSeconds === 0) {
+
+      clearInterval(countDown);
+      
+      readInputNumbers();
+
+      //stampo quantitá numeri ricordati
+      document.querySelector('.content_numbers').innerHTML = `
+         <h1 class="strikes">Hai ricordato ${strikes} numeri</h1>
+      `;
+   }
+}, 1000);
+
 
 /**********
  * FUNZIONI
 ********* */
 
-function pushRandomNumbers() {
+function pushNumbers() {
       
    while (randomNumbers.length < NUMBER_TO_PUSH) {
       
@@ -26,8 +49,6 @@ function pushRandomNumbers() {
          randomNumbers.push(number);
       }
    }
-   // console.log(randomNumbers);
-      
 }
 
 
@@ -45,4 +66,35 @@ function printNumbers() {
       
       contentNumbers.innerHTML += `<h1>${number}</h1>`;
    });
+}
+
+
+function readInputNumbers() {
+   //chiedo il numero per 5 volte e lo pusho
+   for (let i = 0; i < NUMBER_TO_PUSH; i++) {
+      let number;
+      //controllo validitá
+      do {
+         //input numero
+         number = parseInt(prompt('Inserisci un numero da 1 a 100'));
+         if (number < 1 || number > 100) {
+            alert('Inserisci un numero compreso tra 1 e 100! :)')
+         }
+      } while (number < 1 || number > 100);
+      
+      console.log('hai scelto', number);
+
+      checkStrike(number);
+   }
+   // console.log('userInputNumber',userInputNumber);
+}
+
+
+function checkStrike(n) {
+   
+   if (randomNumbers.includes(n)) {
+      
+      //incremento i numeri ricordati
+      strikes++;
+   }
 }
