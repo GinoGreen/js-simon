f0c10fab5569bce9d1e18075120ecf09b322d1fc
GinoGreen <luigiverde97@gmail.com>
2021-11-09 13:05:07 +0100

    init repo

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/**
+ * Generare 5 numeri random e stamparli in html
+ * dopo 30 secondi chiedere all'utente 5 numeri
+ * se i numeri corrispodono ai numeri random, output "hai indovinato 'TOT' numeri"
+ */
